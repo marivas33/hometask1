ea4766480d717ee117a446dfa9373cca503c9888
Marina Vasylakina <vasylakina14@gmail.com>
2018-12-21 21:44:00 +0300

    Initial commit

---
+++ src/hometask1/Start.java
@@ -0,0 +1,20 @@
+package hometask1;
+
+public class Start {
+
+   public static void main(String[] args){
+
+       int addResult = Calculator.add(5 ,7);
+       System.out.println(addResult);
+
+       int subResult = Calculator.sub(100, 35);
+       System.out.println(subResult);
+
+       int divResult = Calculator.div(200, 50);
+       System.out.println(divResult);
+
+       int mulResult = Calculator.mul(33,3);
+       System.out.println(mulResult);
+    }
+
+}
