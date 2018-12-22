df03b00d7f9098f62ddf410889ce9c54cd7a3672
Marina Vasylakina <vasylakina14@gmail.com>
2018-12-22 19:49:39 +0300

    change code

--- src/hometask1/Calculator.java
+++ src/hometask1/Calculator.java
@@ -3,51 +3,34 @@ package hometask1;
 public class Calculator {
 
     public static int add(int x, int y) {
-
-        int result = x + y;
-        return result;
+        return x + y;
     }
-
     public static double add(double x, double y) {
-
-        double result = x + y;
-        return result;
+        return  x + y;
     }
+
 
     public static int sub(int x, int y) {
-
-        int result = x - y;
-        return result;
+        return x - y;
     }
-
     public static double sub(double x, double y) {
-
-        double result = x - y;
-        return result;
+        return  x - y;
     }
+
 
     public static int div(int x, int y){
-
-        int result = x / y;
-        return result;
+        return x / y;
     }
-
     public static double div(double x, double y){
-
-        double result = x / y;
-        return result;
+        return x / y;
     }
+
 
     public static int mul(int x, int y){
-
-        int result = x * y;
-        return result;
+        return x * y;
     }
-
     public static double mul(double x, double y){
-
-        double result = x * y;
-        return result;
+        return x * y;
     }
 
 }
--- src/hometask1/Start.java
+++ src/hometask1/Start.java
@@ -3,18 +3,23 @@ package hometask1;
 public class Start {
 
    public static void main(String[] args){
+       
 
        int addResult = Calculator.add(5 ,7);
        System.out.println(addResult);
+       System.out.println(Calculator.add(7D, 5.3));
 
        int subResult = Calculator.sub(100, 35);
        System.out.println(subResult);
+       System.out.println(Calculator.sub(13D, 5.7));
 
        int divResult = Calculator.div(200, 50);
        System.out.println(divResult);
+       System.out.println(Calculator.div(45D, 5.5));
 
        int mulResult = Calculator.mul(33,3);
        System.out.println(mulResult);
+       System.out.println(Calculator.mul(3D, 3.3));
     }
 
 }
