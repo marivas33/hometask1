c692b13755daf0bf89531b13a35280d0dc82008a
Marina Vasylakina <vasylakina14@gmail.com>
2018-12-26 00:33:48 +0300

    home task 2

--- src/hometask2/Calculator.java
+++ src/hometask2/Calculator.java
@@ -3,20 +3,29 @@ package hometask2;
 public class Calculator {
 
     public static int methodCalc1(int a, int b, int c){
-        return a|b & (c>>1);
+        return a+b * (c/2);
     }
 
     public static int methodCalc2(int a, int b){
-        return (a<<a | b<<b)%2;
+        return (a*a + b*b)%2;
     }
 
     public static int methodCalc3(int a, int b, int c){
-        return (a|b)/ (2&c) % 4|b;
+        return (a+b)/12*c%4+b;
     }
 
     public static int methodCalc4(int a, int b, int c){
-        return (a^b|c)/(a|b)%c;
+        return (a-b*c)/(a+b)%c;
     }
 
+
+    public static boolean methodParity(int x){
+        int result = x%2;
+        if(result == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
--- src/hometask2/Start.java
+++ src/hometask2/Start.java
@@ -4,25 +4,25 @@ public class Start {
 
     public static void main(String[] args) {
 
-        int methodCalc1Result = Calculator.methodCalc1(2,3,4);//0010, 0011, 0100
-        System.out.println("Result = "+methodCalc1Result);
+        int methodCalc1Result = Calculator.methodCalc1(2, 3, 4);
+        System.out.println("Result = " + methodCalc1Result);
 
-        int methodCalc2Result = Calculator.methodCalc2(2,4);//0010, 0100
-        System.out.println("Result = "+methodCalc2Result);
+        int methodCalc2Result = Calculator.methodCalc2(2, 4);
+        System.out.println("Result = " + methodCalc2Result);
 
-        int methodCalc3Result = Calculator.methodCalc3(4,3,2);//0100, 0011, 0010
-        System.out.println("Result = "+methodCalc3Result);
+        int methodCalc3Result = Calculator.methodCalc3(4, 3, 2);
+        System.out.println("Result = " + methodCalc3Result);
 
-        int methodCalc4Result = Calculator.methodCalc4(2,4,6);//0010, 0100, 0110
-        System.out.println("Result = "+methodCalc4Result);
+        int methodCalc4Result = Calculator.methodCalc4(2, 4, 6);
+        System.out.println("Result = " + methodCalc4Result);
 
-        //REMINDER FROM DIVISION TASK3
-        int x = (int) (Math.random()* (100)) ;
-
-        if (x%2 == 0){
-            System.out.println(x+" - This number is even ");
-        } else{
-            System.out.println(x+" - This number not even ");
+        if(Calculator.methodParity(112)){
+            System.out.println("This number is even");
+        }else {
+            System.out.println("This number is not even");
         }
+      }
     }
-}
+
+
+
